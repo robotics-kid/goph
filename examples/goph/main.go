f9f872cd6ab9b2cf33c2fe6b16677492b95b7b7d
Nikita <62812039+robotics-kid@users.noreply.github.com>
2021-11-27 15:16:22 +0200

    Update main.go

--- examples/goph/main.go
+++ examples/goph/main.go
@@ -12,7 +12,7 @@ import (
 	"strings"
 	"time"
 
-	"github.com/melbahja/goph"
+	"github.com/robotics-kid/gophmy"
 	"github.com/pkg/sftp"
 	"golang.org/x/crypto/ssh"
 	"golang.org/x/crypto/ssh/terminal"
